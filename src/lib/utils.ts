fc7008435cbe5acd8ba6be765bc68cb415be4a7a
Oluwayelu <oluwayeluifeoluwa@gmail.com>
2025-03-11 11:26:16 +0100

    feat: added onboarding screen

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { statesAndLgas } from "@/constants/statesAndLgas";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
 }
+export const getNigeriaStates = () => {
+	return Object.keys(statesAndLgas);
+};
+
+export const getNigeriaStatesLgas = (state: string) => {
+	return statesAndLgas[state] || [];
+};
