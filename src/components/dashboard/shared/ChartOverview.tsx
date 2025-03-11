4adfcadb98be2fc8a13ffa6311933c13f9e004ab
Oluwayelu <oluwayeluifeoluwa@gmail.com>
2025-03-11 13:05:16 +0100

    fix: ui changes

--- src/components/dashboard/shared/ChartOverview.tsx
+++ src/components/dashboard/shared/ChartOverview.tsx
@@ -1,88 +1,104 @@
-import React, { useState } from 'react';
-import { usePathname } from 'next/navigation';
+// import React, { useState } from "react";
+import { usePathname } from "next/navigation";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
 } from "@/components/ui/select";
 import { statesAndLgas } from "@/constants/statesAndLgas";
 import { Copy } from "lucide-react";
 import YearlySalesChart from "../shared/YearlySalesChart";
 import TotalSalesChart from "../shared/TotalSalesChart";
-import { chartData } from "@/constants/dummy";
+import { ChartDataType } from "@/constants/dummy";
+import { Dispatch, SetStateAction } from "react";
 
 const referral = {
-    superAgent: 'SBC/004',
-    mainAgent: 'SBC/004/010',
-    subAgent:' SBC/004/010/003'
-}
+	superAgent: "SBC/004",
+	mainAgent: "SBC/004/010",
+	subAgent: " SBC/004/010/003",
+};
 
-const ChartOverview = ({ }) => {
-    const pathName = usePathname();
-    const [location, setLocation] = useState<string>("Lagos");
+type Props = {
+	location: string;
+	chartData: ChartDataType;
+	setLocation: Dispatch<SetStateAction<string>>;
+	filterByLocation: () => void;
+};
 
+const ChartOverview = ({
+	location,
+	chartData,
+	setLocation,
+	filterByLocation,
+}: Props) => {
+	const pathName = usePathname();
+	// const [location, setLocation] = useState<string>("Lagos");
 
-  return (
-    <div>
-      <div className='w-full flex justify-between gap-4 items-center'>
-        <div className='flex gap-2'>
-          <Select value={location} onValueChange={setLocation}>
-            <SelectTrigger className='w-[180px]'>
-              <SelectValue placeholder='Theme' />
-            </SelectTrigger>
-            <SelectContent>
-              {Object.keys(statesAndLgas).map((state, index) => (
-                <SelectItem key={index} value={state}>
-                  {state}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-          <button className='bg-[#A7CC48] py-2 px-6 rounded-md font-medium'>
-            Search
-          </button>
-        </div>
-        <div>
-          <p className='text-sm font-medium flex items-center gap-2'>
-            Referral Code:{" "}
-            <span className='font-semibold'>
-              {pathName.startsWith("/super-agent")
-                ? referral.superAgent
-                : pathName.startsWith("/main-agent")
-                ? referral.mainAgent
-                : pathName.startsWith("/sub-agent") && referral.subAgent}
-            </span>{" "}
-            <Copy className='w-4 h-4' />{" "}
-          </p>
-        </div>
-      </div>
+	return (
+		<div>
+			<div className="w-full flex justify-between gap-4 items-center">
+				<div className="flex gap-2">
+					<Select value={location} onValueChange={setLocation}>
+						<SelectTrigger className="w-[180px]">
+							<SelectValue placeholder="Theme" />
+						</SelectTrigger>
+						<SelectContent>
+							<SelectItem value={"all"}>All states</SelectItem>
+							{Object.keys(statesAndLgas).map((state, index) => (
+								<SelectItem key={index} value={state}>
+									{state}
+								</SelectItem>
+							))}
+						</SelectContent>
+					</Select>
+					<button
+						onClick={filterByLocation}
+						className="bg-[#A7CC48] py-2 px-6 rounded-md font-medium"
+					>
+						Search
+					</button>
+				</div>
+				<div>
+					<p className="text-sm font-medium flex items-center gap-2">
+						Referral Code:{" "}
+						<span className="font-semibold">
+							{pathName.startsWith("/super-agent")
+								? referral.superAgent
+								: pathName.startsWith("/main-agent")
+								? referral.mainAgent
+								: pathName.startsWith("/sub-agent") && referral.subAgent}
+						</span>{" "}
+						<Copy className="w-4 h-4" />{" "}
+					</p>
+				</div>
+			</div>
 
-      {/* chart section */}
-      <div className='w-full flex gap-4'>
-        <div className='w-2/3 border my-4 rounded-md p-4'>
-          <h2 className='font-semibold'>Distributors Sales</h2>
-          <p className='text-xs '>
-            Keep track of Distributors sale every month
-          </p>
-          <div className='w-full'>
-            <YearlySalesChart chartData={chartData} />
-          </div>
-        </div>
-        <div className='w-1/3 border my-4 rounded-md p-4 space-y-4'>
-          <div>
-            <h2 className='font-semibold'>Distributors monitored</h2>
-            <p className='text-xs '>You’re using 67.5% of available spots.</p>
-          </div>
+			{/* chart section */}
+			<div className="w-full flex gap-4">
+				<div className="w-2/3 border my-4 rounded-md p-4">
+					<h2 className="font-semibold">Distributors Sales</h2>
+					<p className="text-xs ">
+						Keep track of Distributors sale every month
+					</p>
+					<div className="w-full">
+						<YearlySalesChart chartData={chartData} />
+					</div>
+				</div>
+				<div className="w-1/3 border my-4 rounded-md p-4 space-y-4">
+					<div>
+						<h2 className="font-semibold">Distributors monitored</h2>
+						<p className="text-xs ">You’re using 67.5% of available spots.</p>
+					</div>
 
-          <div className='w-full'>
-            <TotalSalesChart agents={675} totalAgents={1000} />
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+					<div className="w-full">
+						<TotalSalesChart agents={675} totalAgents={1000} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
 
-export default ChartOverview
+export default ChartOverview;
--- src/components/dashboard/shared/InventoryTable.tsx
+++ src/components/dashboard/shared/InventoryTable.tsx
@@ -1,175 +1,217 @@
-import React, { useState, useMemo, useEffect } from 'react';
-import Link from 'next/link';
+import React, { useState, useMemo, useEffect } from "react";
+import Link from "next/link";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-  TableFooter,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+	TableFooter,
 } from "@/components/ui/table";
 import { IoIosArrowRoundDown } from "react-icons/io";
-import { InventoryProps } from '@/types/dashboard';
+import { InventoryProps } from "@/types/dashboard";
 
-const InventoryTable = ({ inventoryList, thisProduct }: { inventoryList: InventoryProps[], thisProduct:string }) => {
-  const [totals, setTotals] = useState({
-    amountSupplied: 0,
-    amountSold: 0,
-    amountRemaining: 0,
-  });
+const InventoryTable = ({
+	inventoryList,
+	thisProduct,
+}: {
+	inventoryList: InventoryProps[];
+	thisProduct: string;
+}) => {
+	const [totals, setTotals] = useState({
+		amountSupplied: 0,
+		amountSold: 0,
+		amountRemaining: 0,
+	});
 
-  const [filterdProduct, setFilterdProduct] = useState<InventoryProps[]>(inventoryList);
-  
-  useEffect(() => {
-    if (thisProduct === 'ALL') {
-      setFilterdProduct(inventoryList)
-    } else {
-      const sortInventory = inventoryList.filter(item => item.Product === thisProduct);
-      setFilterdProduct(sortInventory)
-    }
-    
-  },[thisProduct, inventoryList])
+	const [filterdProduct, setFilterdProduct] =
+		useState<InventoryProps[]>(inventoryList);
 
-   useMemo(() => {
-    const amountSupply = filterdProduct.reduce((a, b) => a + (b.UnitP*b.QuantityS), 0);
-    const amountSold = filterdProduct.reduce((a, b) => a + (b.UnitP*b.quantityS), 0 );
-    const amountRemain = filterdProduct.reduce((a, b) => a + (b.UnitP *(b.QuantityS-b.quantityS)), 0);
-    
-    return setTotals({
-      amountSupplied: amountSupply,
-      amountSold: amountSold,
-      amountRemaining:amountRemain,
-    })
-    // console.log(amountSupply)
-  },[filterdProduct])
-  
-    return (
-      <div className=' border border-slate-100 shadow p-2'>
-        <div className='w-full p-3 border-b flex justify-between items-center'>
-          <div className='flex items-center space-x-4'>
-            <h3 className='font-semibold'>Inventory Management</h3>
-            <div className='flex items-center space-x-2'>
-              <div className='flex items-center space-x-0.5'>
-                <div className='p-1 rounded-full bg-green-400'></div>
-                <span className='text-xs'>Current Product</span>
-              </div>
-              <div className='flex items-center space-x-0.5'>
-                <div className='p-1 rounded-full bg-yellow-500'></div>
-                <span className='text-xs'>Brought Forward</span>
-              </div>
-              <div className='flex items-center space-x-0.5'>
-                <div className='p-1 rounded-full bg-red-500'></div>
-                <span className='text-xs'>Recalled Product</span>
-              </div>
-            </div>
-          </div>
-          <div>
-            <Link href='/super-agent' className='text-xs font-bold underline'> View Sales performance report</Link>
-          </div>
-        </div>
-        <div className='max-h-[60vh] overflow-auto relative'>
-          <Table>
-            <TableHeader className=''>
-              <TableRow className=''>
-                <TableHead className='flex items-center justify-center space-x-1'>
-                  <span>Product</span>
-                  <IoIosArrowRoundDown />
-                </TableHead>
-                <TableHead className='text-center'>Batch ID</TableHead>
-                <TableHead className='text-center'>Entry Date</TableHead>
-                <TableHead className='text-center font-extrabold text-green-800'>
-                  Quantity Supplied
-                </TableHead>
-                <TableHead className='text-center'>Unit Price</TableHead>
-                <TableHead className='text-center font-bold text-slate-800 bg-green-600/30'>
-                  Supplied Receivable
-                </TableHead>
-                <TableHead className='text-center'>Quantity Sold</TableHead>
-                <TableHead className='text-center font-bold text-slate-800 bg-green-400/25'>
-                  Amount Sold
-                </TableHead>
-                <TableHead className='text-center'>
-                  Quantity Remaining
-                </TableHead>
-                <TableHead className='text-center font-bold text-slate-800 bg-yellow-600/25'>
-                  Amount Remaining
-                </TableHead>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {filterdProduct.map((list, key) => {
-                return (
-                  <TableRow key={key}>
-                    <TableCell className='text-center cursor-pointer '>
-                      <div className='flex items-center space-x-1'>
-                        {key % 2 === 0 ? (
-                          <div className='p-1 rounded-full bg-green-400'></div>
-                        ) : (
-                          <div className='p-1 rounded-full bg-yellow-500'></div>
-                        )}{" "}
-                        <span>{list.Product}</span>
-                      </div>
-                    </TableCell>
-                    <TableCell className='text-center font-semibold'>
-                      {list.BatchID}
-                    </TableCell>
-                    <TableCell className='text-center'>{list.EntryD}</TableCell>
+	useEffect(() => {
+		if (thisProduct === "ALL") {
+			setFilterdProduct(inventoryList);
+		} else {
+			const sortInventory = inventoryList.filter(
+				(item) => item.Product === thisProduct
+			);
+			setFilterdProduct(sortInventory);
+		}
+	}, [thisProduct, inventoryList]);
 
-                    <TableCell className='text-center'>
-                      {list.QuantityS}
-                    </TableCell>
-                    <TableCell className='font-semibold text-center'>
-                      &#8358;{list.UnitP}
-                    </TableCell>
-                    <TableCell className='font-semibold text-center bg-green-600/30 '>
-                      &#8358;{list.QuantityS * list.UnitP}
-                    </TableCell>
-                    <TableCell className='text-center'>
-                      {list.quantityS}
-                    </TableCell>
-                    <TableCell className='font-semibold bg-green-400/20 text-center'>
-                      &#8358;{list.quantityS * list.UnitP}
-                    </TableCell>
-                    <TableCell className='text-center'>
-                      {list.QuantityS - list.quantityS}
-                    </TableCell>
-                    <TableCell className='font-semibold bg-yellow-600/20 text-center'>
-                      &#8358;{(list.QuantityS - list.quantityS) * list.UnitP}
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-            </TableBody>
-          </Table>
-        </div>
-        <footer className=''>
-          <Table>
-            {/* */}
-            <TableFooter className='relative bottom-1'>
-              <TableRow className=' bg-slate-200 '>
-                <TableCell className='text-center w-[115px]'></TableCell>
-                <TableCell className='text-center w-[89px]'></TableCell>
-                <TableCell className='text-center w-[93px]'></TableCell>
-                <TableCell className='text-center w-[124px]'></TableCell>
-                <TableCell className='text-center w-[84px]'></TableCell>
-                <TableCell className='text-center w-[124px] font-bold text-base text-slate-700'>
-                  &#8358;{totals.amountSupplied}
-                </TableCell>
-                <TableCell className='text-center w-[106px]'></TableCell>
-                <TableCell className='text-center w-[104px] font-bold text-base text-slate-700'>
-                  &#8358;{totals.amountSold}
-                </TableCell>
-                <TableCell className='text-center w-[135px]'></TableCell>
-                <TableCell className='text-center w-[135px] font-bold text-base text-slate-700'>
-                  &#8358;{totals.amountRemaining}
-                </TableCell>
-              </TableRow>
-            </TableFooter>
-          </Table>
-        </footer>
-      </div>
-    );
-}
+	useMemo(() => {
+		const amountSupply = filterdProduct.reduce(
+			(a, b) => a + b.UnitP * b.QuantityS,
+			0
+		);
+		const amountSold = filterdProduct.reduce(
+			(a, b) => a + b.UnitP * b.quantityS,
+			0
+		);
+		const amountRemain = filterdProduct.reduce(
+			(a, b) => a + b.UnitP * (b.QuantityS - b.quantityS),
+			0
+		);
+
+		return setTotals({
+			amountSupplied: amountSupply,
+			amountSold: amountSold,
+			amountRemaining: amountRemain,
+		});
+		// console.log(amountSupply)
+	}, [filterdProduct]);
+
+	return (
+		<div className=" border border-slate-100 shadow p-2">
+			<div className="w-full p-3 border-b flex justify-between items-center">
+				<div className="flex items-center space-x-4">
+					<h3 className="font-semibold">Inventory Management</h3>
+					<div className="flex items-center space-x-2">
+						<div className="flex items-center space-x-0.5">
+							<div className="p-1 rounded-full bg-green-400"></div>
+							<span className="text-xs">Current Product</span>
+						</div>
+						<div className="flex items-center space-x-0.5">
+							<div className="p-1 rounded-full bg-yellow-500"></div>
+							<span className="text-xs">Brought Forward</span>
+						</div>
+						<div className="flex items-center space-x-0.5">
+							<div className="p-1 rounded-full bg-red-500"></div>
+							<span className="text-xs">Recalled Product</span>
+						</div>
+					</div>
+				</div>
+				<div>
+					<Link href="/super-agent" className="text-xs font-bold underline">
+						{" "}
+						View Sales performance report
+					</Link>
+				</div>
+			</div>
+			<div className="max-h-[60vh] overflow-auto relative">
+				<Table>
+					<TableHeader className="">
+						<TableRow className="text-nowrap">
+							<TableHead className="flex items-center justify-center space-x-1">
+								<span>Product</span>
+								<IoIosArrowRoundDown />
+							</TableHead>
+							<TableHead className="text-center">Batch ID</TableHead>
+							<TableHead className="text-center">Entry Date</TableHead>
+							<TableHead className="text-center font-extrabold text-green-800">
+								Quantity Supplied
+							</TableHead>
+							<TableHead className="text-center">Unit Price</TableHead>
+							<TableHead className="text-center font-bold text-slate-800 bg-green-600/30">
+								Supplied Receivable
+							</TableHead>
+							<TableHead className="text-center">Quantity Sold</TableHead>
+							<TableHead className="text-center font-bold text-slate-800 bg-green-400/25">
+								Amount Sold
+							</TableHead>
+							<TableHead className="text-center">Quantity Remaining</TableHead>
+							<TableHead className="text-center font-bold text-slate-800 bg-yellow-600/25">
+								Amount Remaining
+							</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{filterdProduct.map((list, key) => {
+							return (
+								<TableRow key={key} className="text-nowrap">
+									<TableCell className="text-center cursor-pointer ">
+										<div className="flex items-center space-x-1">
+											{key % 2 === 0 ? (
+												<div className="p-1 rounded-full bg-green-400"></div>
+											) : (
+												<div className="p-1 rounded-full bg-yellow-500"></div>
+											)}{" "}
+											<span>{list.Product}</span>
+										</div>
+									</TableCell>
+									<TableCell className="text-center font-semibold">
+										{list.BatchID}
+									</TableCell>
+									<TableCell className="text-center">{list.EntryD}</TableCell>
+									<TableCell className="text-center">
+										{list.QuantityS.toLocaleString()}
+									</TableCell>
+									<TableCell className="font-semibold text-center">
+										&#8358;{list.UnitP.toLocaleString()}
+									</TableCell>
+									<TableCell className="font-semibold text-center bg-green-600/30 ">
+										&#8358;{(list.QuantityS * list.UnitP).toLocaleString()}
+									</TableCell>
+									<TableCell className="text-center">
+										{list.quantityS.toLocaleString()}
+									</TableCell>
+									<TableCell className="font-semibold bg-green-400/20 text-center">
+										&#8358;{(list.quantityS * list.UnitP).toLocaleString()}
+									</TableCell>
+									<TableCell className="text-center">
+										{(list.QuantityS - list.quantityS).toLocaleString()}
+									</TableCell>
+									<TableCell className="font-semibold bg-yellow-600/20 text-center">
+										&#8358;
+										{(
+											(list.QuantityS - list.quantityS) *
+											list.UnitP
+										).toLocaleString()}
+									</TableCell>
+								</TableRow>
+							);
+						})}
+					</TableBody>
+					<TableFooter className="">
+						<TableRow className=" bg-slate-200 ">
+							<TableCell className="text-center w-[124px] font-bold text-base text-slate-700">
+								Total
+							</TableCell>
+							<TableCell
+								colSpan={4}
+								className="text-center w-[115px]"
+							></TableCell>
+							<TableCell className="text-center w-[124px] font-bold text-base text-slate-700">
+								&#8358;{totals.amountSupplied.toLocaleString()}
+							</TableCell>
+							<TableCell className="text-center w-[106px]"></TableCell>
+							<TableCell className="text-center w-[104px] font-bold text-base text-slate-700">
+								&#8358;{totals.amountSold.toLocaleString()}
+							</TableCell>
+							<TableCell className="text-center w-[135px]"></TableCell>
+							<TableCell className="text-center w-[135px] font-bold text-base text-slate-700">
+								&#8358;{totals.amountRemaining.toLocaleString()}
+							</TableCell>
+						</TableRow>
+					</TableFooter>
+				</Table>
+			</div>
+			{/* <footer className="">
+				<Table>
+					<TableFooter className="relative bottom-1">
+						<TableRow className=" bg-slate-200 ">
+							<TableCell className="text-center w-[115px]"></TableCell>
+							<TableCell className="text-center w-[89px]"></TableCell>
+							<TableCell className="text-center w-[93px]"></TableCell>
+							<TableCell className="text-center w-[124px]"></TableCell>
+							<TableCell className="text-center w-[84px]"></TableCell>
+							<TableCell className="text-center w-[124px] font-bold text-base text-slate-700">
+								&#8358;{totals.amountSupplied.toLocaleString()}
+							</TableCell>
+							<TableCell className="text-center w-[106px]"></TableCell>
+							<TableCell className="text-center w-[104px] font-bold text-base text-slate-700">
+								&#8358;{totals.amountSold.toLocaleString()}
+							</TableCell>
+							<TableCell className="text-center w-[135px]"></TableCell>
+							<TableCell className="text-center w-[135px] font-bold text-base text-slate-700">
+								&#8358;{totals.amountRemaining.toLocaleString()}
+							</TableCell>
+						</TableRow>
+					</TableFooter>
+				</Table>
+			</footer> */}
+		</div>
+	);
+};
 
 export default InventoryTable;
